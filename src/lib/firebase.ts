445504179899939a517d49ed94bcd39f9b76d2ba
Pravin Sankhat <sankhatpravin121@gmail.com>
2025-09-03 14:09:58 +0000

    have new firebase project Arena Ace 2  se

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyB6XufbP1uWMzbyOMAiGws4s-_Ed8RwLTI",
   authDomain: "arena-ace-2.firebaseapp.com",
   projectId: "arena-ace-2",
-  storageBucket: "arena-ace-2.appspot.com",
+  storageBucket: "arena-ace-2.firebasestorage.app",
   messagingSenderId: "724343463324",
   appId: "1:724343463324:web:6cd4d755ea96d9f65b3c59"
 };
