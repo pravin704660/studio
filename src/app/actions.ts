95defa704a5b86b7da948c6025b0b5e56a66b384
pravin704660 <70677304+pravin704660@users.noreply.github.com>
2025-09-20 15:04:31 +0530

    Update actions.ts

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -201,7 +201,6 @@ export async function createOrUpdateTournament(
       throw new Error("Tournament data is missing.");
     }
     
-      imageUrl = url;
     } else if (!imageUrl && !tournamentData.id) {
         // Only set default if no image URL is present and it's a new tournament
         if (tournamentData.isMega) {
