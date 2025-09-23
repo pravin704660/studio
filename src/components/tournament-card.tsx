52c1f84bb160b9145030a69579b925321fa288dc
pravin704660 <70677304+pravin704660@users.noreply.github.com>
2025-09-23 13:55:01 +0530

    Update tournament-card.tsx

--- src/components/tournament-card.tsx
+++ src/components/tournament-card.tsx
@@ -252,8 +252,6 @@ export default function TournamentCard({
           {isJoining ? <Spinner /> : showCredentials ? "Joined" : "Join Now"}
         </Button>
       </CardFooter> 
-      <script async="async" data-cfasync="false" src="//pl27417148.revenuecpmgate.com/d0c305ac9fb0548d6209c0be9eb7bf6d/invoke.js"></script>
-<div id="container-d0c305ac9fb0548d6209c0be9eb7bf6d"></div>
     </Card>
   );
 }
